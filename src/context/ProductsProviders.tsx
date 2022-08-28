2ae44ddb0d36b5e7929213a0746a81a319517375
Esteban Gutierrez <esteb.gtz.mtz@hotmail.com>
2022-08-28 01:35:52 -0500

    add query to get peoducts

--- src/context/ProductsProviders.tsx
+++ src/context/ProductsProviders.tsx
@@ -1,13 +1,24 @@
-import { useState } from 'react';
-import { IProductsProviderProps } from '../interfaces/interfaces';
+import { useEffect, useState } from 'react';
+import { IProductsProviderProps, IProductsResponse } from '../interfaces/interfaces';
+import { getProducts } from '../services/ProductsService';
 import { ProductsContext } from './ProductsContext';
 
 const ProductsProviders = ({children}: IProductsProviderProps) => {
 
-  const [test, setTest] = useState('context test its working correctly')
+  useEffect(() => {
+    getProductsQuery()
+  }, [])
+  
+  const getProductsQuery = async ()=>{
+    const products = await getProducts();
+    setProducts(products)
+  }
+
+  const [products, setProducts] = useState<IProductsResponse[]>([])
+  const [test] = useState('context test its working correctly')
 
   return (
-    <ProductsContext.Provider value={{test}}>
+    <ProductsContext.Provider value={{test, products}}>
       {children}
     </ProductsContext.Provider>
   )
